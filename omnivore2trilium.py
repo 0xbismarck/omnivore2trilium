46f265e9b36766570001f4c549f998aea27517dd
0xbismarck <0xbismarck@gmail.com>
2024-02-24 15:47:29 -0500

    Standardized function headers and removed extra print statements.

--- omnivore2trilium.py
+++ omnivore2trilium.py
@@ -7,23 +7,22 @@ import sys
 import pathlib
 
 
-def auth_omnivore(key):
+def authOmnivore(key):
     omnivoreql_client = OmnivoreQL(str(key))
     # print (omnivoreql_client)
     return omnivoreql_client
 
-def auth_trilium(key):
+def authTrilium(key):
     server_url = 'http://localhost:37840'
     ea = ETAPI(server_url, key)
-    #print(ea.app_info())
+    # print(ea.app_info())
     return ea
 
-def print_profile(omnivoreql_client):
+def printOmnivoreProfile(omnivoreql_client):
     print(omnivoreql_client.get_profile())
 
-def fetch_articles(omnivoreql_client, queryString, articleLimit):
-    #articles = omnivoreql_client.get_articles()
-    #print(articles["search"])
+# Query the Omnivore API for and return article highlights.
+def fetchArticles(omnivoreql_client, queryString, articleLimit):
     article_notes = []
     profile = omnivoreql_client.get_profile()
 
@@ -36,35 +35,30 @@ def fetch_articles(omnivoreql_client, queryString, articleLimit):
     for item in articles['search']['edges']:
         slug = item['node']['slug']
         article = omnivoreql_client.get_article(username, slug)
-        article_notes.append(note_dict(article))
-        #print (count , ": ", articles['article']['article']['highlights'])
+        article_notes.append(buildNoteDictionary(article))
+        # print (count , ": ", articles['article']['article']['highlights'])
         count+=1
-    print ("Count == ",count)
+    # print ("Count == ",count)
     return article_notes
 
 # builds a dictionary object with items neededed for exporting.
-def note_dict(article):
-    #print(article)
+def buildNoteDictionary(article):
     note = {}
     note["title"] = article['article']['article']['title']
     note["url"] = article['article']['article']['url']
     note["author"] = article['article']['article']['author']
     note["slug"] = article['article']['article']['slug']
-    highlights = extract_highlights(article['article']['article']['highlights'])
+    highlights = extractHighlights(article['article']['article']['highlights'])
     note["highlights"] = highlights
-    #print (article['article']['article']['labels'])
-    note["labels"] = get_labels(article['article']['article']['labels'])
-    #print("Extracted highlights:")
-    #print(note["highlights"])
-    print(note)
+    # print (article['article']['article']['labels'])
+    note["labels"] = getLabels(article['article']['article']['labels'])
     return note
 
 # given an article, it will return a list of highlights/annotations.
-def extract_highlights(highlights):
+def extractHighlights(highlights):
     article_highlights = []
     if highlights:
         #print ( "====================")
-        #print (highlights)
         for highlight in highlights:
             #print (highlight)
             #print (highlight["quote"])
@@ -75,32 +69,28 @@ def extract_highlights(highlights):
                 article_highlights.append(highlight["annotation"])
         #print ( "====================")
         #print (highlight["annotation"])
-    #else:
-    #    print ("Empty")
     return article_highlights
 
-def get_labels(label_list):
+# get the article's labels
+def getLabels(label_list):
     labels = ["omnivoreHighlight"]
     for label in label_list:
         labels.append(label["name"])
-    
     return labels
 
 def createNote(tclient, myNotes, pNoteId ):
-    # print(tclient.app_info())
     for note in myNotes:
-        # print ("SLUG - "+note["slug"])
         slugHash = hashlib.sha256(note["slug"].encode()).hexdigest()
-        # print ("SLUG hash - "+slugHash)
         res = tclient.create_note( parentNoteId =  pNoteId,
         title = note["title"],
         type="text",
         content = formatNoteContent(note),
         noteId =  slugHash
         )
-        print (res)
+        # print (res)
         addLabels(tclient, note, slugHash)
 
+# Format the content before adding it Trilium.
 def formatNoteContent(note):
     content = ''
     if note["author"]:
@@ -113,16 +103,16 @@ def formatNoteContent(note):
 # adding labels to a note. at minimal there will be an #omnivoreHighlight label.
 def addLabels(tclient, note, noteid):
     for label in note["labels"]:
-        print ("*********************"+label)
+        # print ("*********************"+label)
         res = tclient.create_attribute(
             noteId=noteid,
             attributeId=hashlib.sha256(label.encode()).hexdigest(),
             type='label',
-            name=label, #.replace(' ', ''),
+            name=label,
             value=None,
             isInheritable=False
             )
-        print (res)
+        # print (res)
 
 # Method loads keys from file. Each key needs the format:
 # omnivore:omnivore_key
@@ -134,32 +124,28 @@ def loadKeys(fileName):
     if pathlib.Path(fileName).is_file():
         keyFile = open(fileName, 'r')
         lines = keyFile.readlines()
-
         for line in lines:
             if line.startswith('trilium:'):
-                # print ("trilium key - "+line)
                 trilium_key = line[8:].strip()
                 # print ("t key = " + trilium_key)
             elif line.startswith('omnivore:'):
-                # print ("omnivore key - "+line)
                 omnivore_key = line[9:].strip()
                 # print ("o key - "+ omnivore_key)
     return omnivore_key, trilium_key
 
 # builds the query string for the Omnivore Query engine. 
 def queryStringBuilder(args):
-    #base query string
+    # base query string
     queryStr = "has:highlights" # "in:all AND updated:"+dateStr+".. AND has:highlights"
-    #in:inbox  in:all  in:archive
+    # in:inbox  in:all  in:archive
     queryStr+= f" AND in:{args.archive}"
-    #add date string
+    # add date string
     if args.days:
-        print ("Args.days == "+ str(args.days))
         today = DT.date.today()
         past_date = today - DT.timedelta(days=args.days)
         queryStr+= f" AND updated:{past_date.year}-{past_date.month}-{past_date.day}.."
     
-    print ("queryStr = "+ queryStr)
+    # print ("queryStr = "+ queryStr)
     return queryStr
 
 if __name__ == "__main__":
@@ -180,10 +166,9 @@ if __name__ == "__main__":
     okey, tkey = loadKeys(args.keys)
     if okey is None or tkey is None:
         sys.exit("Error: both keys are required.")
-    oclient = auth_omnivore(okey)
-    tclient = auth_trilium(tkey)
+    oclient = authOmnivore(okey)
+    tclient = authTrilium(tkey)
     queryString = queryStringBuilder(args)
-    #print (tclient)
-    #print_profile(oclient)
-    myNotes = fetch_articles(oclient, queryString, args.limit)
+
+    myNotes = fetchArticles(oclient, queryString, args.limit)
     createNote(tclient, myNotes, args.parentNoteId)
